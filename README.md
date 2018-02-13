7e934fda5302bb2c3e5548453b7db59468efb5fc
Wxiaosheng <287915338@qq.com>
2018-02-13 12:45:05 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # node-server
 这是使用纯 nodejs 搭建的后台服务器，提供简要的API接口以及后台数据的管理系统
+
+快速开始
+
+首先确保你当前服务器上已经搭建好了node环境，并注意是否自己开发时的版本一致
+
+然后 git clone https://github.com/Wxiaosheng/node-server.git 
+
+cd node-server 
+node server.js
+
+这是就起好了当前的 node 服务器，并且可以通过你的工网IP访问，并且得到结果
+
+注意：
+    如果 localhost 能够访问而工网IP不能访问，请参照[]
---
+++ server.js
@@ -0,0 +1,28 @@
+/**
+ * 使用http模块向客户端返回响应消息
+ */
+
+var http = require('http');
+
+//1创建一个Web服务器 —— 创建一个面包售货员
+var server = http.createServer();
+
+//2让Web服务器能够处理客户端连接请求——岗前培训
+server.on('request',function(request, response){
+    console.log('Web服务器接收到一个HTTP请求');
+    console.log('Web服务器开始构建响应消息');
+
+    //设置响应状态码 和 响应头部
+    response.writeHead(200, {
+        'Content-Type': 'text/html;charset=UTF-8',
+        'Cache-Control': 'no-cache'
+    });
+    response.write('<h1>I am Node.js Server</h1>');  //输出响应主体
+    response.write('<p>欢迎访问我的Web服务器</p>');
+    response.end();  //结束响应主体的输出
+})
+
+//3让Web服务器开始监听指定端口——开始上岗
+server.listen(9090, '0.0.0.0', function(){
+    console.log('Web服务器开始监听客户端请求')
+})
